17b7e180dec70ee15566a62e4c8f72f5c1334f9b
Yan Gabriel <yan.esteves@cinnecta.com>
2024-01-11 11:48:09 -0300

    criando layout de teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import './App.css'
 
 function App() {
-
-  return <span className='hello-vivo'>Hello Vivo!</span>
+  return (
+    <div className="form-container">
+      <span className="ask-title">
+        Descubra os melhores planos e ofertas para sua Empresa
+      </span>
+      <span className="ask-subtitle">
+        Com poucas informações você receberá serviços da Vivo selecionados
+        exclusivamente.
+      </span>
+      <div className="button-container">
+        <button className="button-base button-small light-outline">
+          {'<'}
+        </button>
+        <button className="button-base button-large light-outline">
+          Avançar
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
